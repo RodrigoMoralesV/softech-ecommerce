2f6ca6a9d0ae8b03744915c340b085a469053f35
richard RR <richard520ruiz@gmail.com>
2024-06-25 22:39:44 -0500

    agrege la funcion de carrito eliminar productos

--- app/Http/Controllers/CarritoController.php
+++ app/Http/Controllers/CarritoController.php
@@ -11,7 +11,7 @@ class CarritoController extends Controller
     {
         // Obtener el carrito desde la sesión
         $cart = $request->session()->get('cart', []);
-        
+
         $total = 0;
         $totalItems = 0;
         $cartItems = [];
@@ -19,9 +19,9 @@ class CarritoController extends Controller
             $product = Producto::find($id_producto);
             if ($product) {
                 $productDetails = [
-                    'image' => $product->imagen_miniatura_producto,
+                    'image' => $product->galeria_imagenes_productos,
                     'name' => $product->descripcion_producto,
-                    'price'=> $product->valor_unitario,
+                    'price' => $product->valor_unitario,
                     'value' => $product->Descuento_producto,
                     'quantity' => $item['quantity'],
                     'id_producto' => $id_producto // Agregar el id_producto al detalle del producto
@@ -33,9 +33,9 @@ class CarritoController extends Controller
                 $totalItems += $item['quantity']; // Contar la cantidad total de productos
             }
         }
-        
+
         // Pasar $cartItems, $total y $totalItems a la vista
-        return view('cart/cart', compact('cartItems', 'total', 'totalItems'));
+        return view('cart.cart', compact('cartItems', 'total', 'totalItems'));
     }
 
     public function addToCart(Request $request, $id_producto)
@@ -74,19 +74,47 @@ class CarritoController extends Controller
     {
         // Obtener el carrito desde la sesión
         $cart = $request->session()->get('cart', []);
-        
+    
         // Verificar si el producto existe en el carrito
         if (isset($cart[$id_producto])) {
             // Eliminar el producto del carrito
             unset($cart[$id_producto]);
-            
+    
             // Guardar el carrito actualizado en la sesión
             $request->session()->put('cart', $cart);
-            
-            return response()->json(['message' => 'Producto eliminado del carrito'], 200);
+    
+            // Calcular el nuevo total y los elementos del carrito
+            $total = 0;
+            $totalItems = 0;
+            $cartItems = [];
+            foreach ($cart as $id => $item) {
+                $product = Producto::find($id);
+                if ($product) {
+                    $productDetails = [
+                        'id_producto' => $id,
+                        'image' => $product->galeria_imagenes_productos,
+                        'name' => $product->descripcion_producto,
+                        'price' => $product->valor_unitario,
+                        'value' => $product->Descuento_producto,
+                        'quantity' => $item['quantity'],
+                    ];
+                    $cartItems[] = $productDetails;
+    
+                    $subtotal = ($product->valor_unitario - $product->Descuento_producto) * $item['quantity'];
+                    $total += $subtotal;
+                    $totalItems += $item['quantity'];
+                }
+            }
+    
+            return response()->json([
+                'message' => 'Producto eliminado del carrito',
+                'cartItems' => $cartItems,
+                'total' => $total,
+                'totalItems' => $totalItems
+            ], 200);
         } else {
             return response()->json(['message' => 'Producto no encontrado en el carrito'], 404);
         }
     }
+    
 }
-?>
--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -14,42 +14,49 @@
         <div class="row">
             <div class="col-lg-10 offset-lg-1">
                 <div class="cart_container">
-                    <div class="cart_title">Carrito de Compras</div>
+                    <div class="cart_title">Tus Compras</div>
                     <div class="cart_items">
                         <ul class="cart_list">
                             @if (count($cartItems) > 0)
                                 @foreach ($cartItems as $item)
-                                    <li class="cart_item clearfix">
+                                    <li class="cart_item clearfix" id="cart-item-{{ $item['id_producto'] }}">
                                         <div class="cart_item_image"><img src="{{ $item['image'] }}" alt=""></div>
                                         <div class="cart_item_info d-flex flex-md-row flex-column justify-content-between">
-                                            <div class="cart_item_name cart_info_col">
+                                            <div class="cart_info_col">
                                                 <div class="cart_item_title">Producto</div>
                                                 <div class="cart_item_text">{{ $item['name'] }}</div>
                                             </div>
-                                            <div class="cart_item_color cart_info_col">
-                                                <div class="cart_item_title">Color</div>
-                                                <div class="cart_item_text"><span style="background-color:#999999;"></span>Color</div>
-                                            </div>
-                                            <div class="cart_item_quantity cart_info_col">
+                                            <div class="cart_info_col">
                                                 <div class="cart_item_title">Cantidad</div>
                                                 <div class="cart_item_text">{{ $item['quantity'] }}</div>
                                             </div>
-                                            <div class="cart_item_price cart_info_col">
+                                            <div class="cart_info_col">
                                                 <div class="cart_item_title">Precio</div>
-                                                <div class="cart_item_text">${{ $item['price'] }}</div>
+                                                <div class="cart_item_text">${{ number_format($item['price'], 0, ',', '.') }}</div>
                                             </div>
-                                            <div class="cart_item_total cart_info_col">
+                                            <div class="cart_info_col">
                                                 <div class="cart_item_title">Total</div>
-                                                <div class="cart_item_text">${{ ($item['price'] - $item['value']) * $item['quantity'] }}</div>
+                                                <div class="cart_item_text">${{ number_format(($item['price'] - $item['value']) * $item['quantity'], 0, ',', '.') }}</div>
+                                            </div>
+                                            <div class="cart_info_col">
+                                                <!-- Formulario para eliminar producto del carrito -->
+                                                <form class="removeFromCartForm" data-product-id="{{ $item['id_producto'] }}" action="{{ route('cart.remove', ['id_producto' => $item['id_producto']]) }}" method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="button cart_button_clear">Eliminar</button>
+                                                </form>
                                             </div>
                                         </div>
                                     </li>
                                 @endforeach
                             @else
-                                <li class="cart_item clearfix ">
-                                    <div class="cart_item_info d-flex flex-md-row flex-column justify-content-between ">
-                                        <div class="cart_item_name cart_info_col">
-                                            <div class="text-center"><h4>Su carrito de compra se encuentra vacío</h4></div>
+                                <li class="cart_item clearfix">
+                                    <div class="cart_item_info d-flex flex-md-row flex-column justify-content-between">
+                                        <div class="cart_info_col">
+                                            <div class="text-center">
+                                                <h4>Su carrito de compra se encuentra vacío</h4>
+                                                <p>Agregue productos a su carrito para comenzar a comprar.</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </li>
@@ -61,17 +68,12 @@
                         <div class="order_total">
                             <div class="order_total_content text-md-right">
                                 <div class="order_total_title">Total</div>
-                                <div class="order_total_amount">${{ $total }}</div>
+                                <div class="order_total_amount" id="order_total_amount">${{ number_format($total, 0, ',', '.') }}</div>
                             </div>
                         </div>
                         <div class="cart_buttons">
-                            <!-- Formulario para eliminar producto del carrito -->
-							<form action="{{ route('cart.remove', ['id_producto' => $item['id_producto']]) }}" method="POST">
-							@csrf
-                                @method('DELETE') <!-- Agrega el método DELETE -->
-                                <button type="submit" class="button cart_button_clear">Eliminar</button>
-                            </form>
-                            <!-- Enlace para proceder al pago -->
+                            <!-- Botón para proceder al pago -->
+                            <button type="button" class="button cart_button_checkout">Comprar</button>
                         </div>
                     @endif
                 </div>
@@ -80,29 +82,45 @@
     </div>
 </div>
 
-<script>
-    // Escuchar el evento submit del formulario
-    document.getElementById('removeFromCartForm').addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevenir el comportamiento por defecto del formulario
+<div id="productDeletedAlert" class="alert alert-success" style="display: none;">Producto eliminado</div>
 
-        // Enviar la solicitud DELETE utilizando Ajax
-        fetch(this.action, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRF-TOKEN': '{{ csrf_token() }}' // Enviar el token CSRF
-            }
-        })
-        .then(response => {
-            if (response.ok) {
-                // Si la eliminación fue exitosa, puedes realizar alguna acción adicional si es necesario
-                console.log('Producto eliminado correctamente');
-                // Puedes redirigir o actualizar la página si es necesario
-            } else {
-                console.error('Error al eliminar producto');
-            }
-        })
-        .catch(error => console.error('Error en la solicitud:', error));
+<script>
+    document.querySelectorAll('.removeFromCartForm').forEach(form => {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault(); // Prevenir el comportamiento por defecto del formulario
+
+            const productId = this.dataset.productId;
+            const formData = new FormData(this);
+            const actionUrl = this.action;
+
+            fetch(actionUrl, {
+                method: 'DELETE',
+                headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                    'Accept': 'application/json'
+                },
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.message === 'Producto eliminado del carrito') {
+                    // Eliminar el producto del DOM
+                    document.getElementById(`cart-item-${productId}`).remove();
+                    
+                    // Actualizar el total y el número de elementos en el carrito
+                    document.getElementById('order_total_amount').textContent = `$${new Intl.NumberFormat('es-ES').format(data.total)}`;
+
+                    // Mostrar la alerta de eliminación
+                    document.getElementById('productDeletedAlert').style.display = 'block';
+                    setTimeout(() => {
+                        document.getElementById('productDeletedAlert').style.display = 'none';
+                    }, 2000);
+                } else {
+                    console.error('Error al eliminar producto');
+                }
+            })
+            .catch(error => console.error('Error en la solicitud:', error));
+        });
     });
 </script>
 
