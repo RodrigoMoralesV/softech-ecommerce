04583dd286413533d6586f7d69b662795d675ea4
jscortes23 <js.199911@gmail.com>
2024-08-22 11:48:29 -0500

    Crear servicio de envio de correos

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,21 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\WelcomeSoftech;
 use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class LoginController extends Controller
 {
     // Retornar vista del login
-    public function login(){
+    public function login()
+    {
+        Mail::to(["js.199911@gmail.com", "moralesvarelarodrigo0@gmail.com", "mv.miguelv@gmail.com", "lmiguel.herl@gmail.com"])->send(new WelcomeSoftech());
         return view('login.login');
     }
 
     // Gestiona el login
-    public function check(Request $request){
+    public function check(Request $request)
+    {
         $credentials = $request->only('email', 'password');
 
-        if(Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
             return redirect()->intended('index');
