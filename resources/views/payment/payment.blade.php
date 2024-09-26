5d83a1acc85719353bedab5845fcb770d83d2ed3
Rodrigo Morales Varela <moralesvarelarodrigo0@gmail.com>
2024-09-26 09:40:37 -0500

    Pago

--- resources/views/payment/payment.blade.php
+++ resources/views/payment/payment.blade.php
@@ -9,42 +9,43 @@
 
 @section('content')
 
-    <!-- Login Form -->
-    <div class="contact_form" style="height: calc(75vh - 110px )">
-        <div class="container" >
-            <div class="row">
-                <div class="col-lg-10 offset-lg-1">
-                    <div class="contact_form_container" >
-                        <div class="contact_form_title d-flex justify-content-center">Haz tu Pago</div>
-                        <form>
-                          <div id="paypal-buttons"></div>
-                          <script>
-                            paypal.Buttons({
-                              createOrder: function(data, actions) {
-                                return actions.order.create({
-                                  purchase_units: [{
-                                    amount: {
-                                      value: '{{$totalUSD}}'
-                                    }
-                                  }]
-                                });
-                              },
-                              onApprove: function(data, actions) {
-                                return actions.order.capture().then(function(details) {
-                                  alert('Transacción completada por ' + details.payer.name.given_name);
-                                });
-                              },
-                              onError: function(err) {
-                                console.error(err);
-                              }
-                            }).render('#paypal-buttons');
-                          </script>
-                        </div>
-                        </form>
-                    </div>
+  <!-- Payment Form -->
+    <div class="contact_form" style="height: calc(75vh - 110px)">
+      <div class="container" >
+        <div class="row">
+          <div class="col-lg-10 offset-lg-1">
+            <div class="contact_form_container" >
+              <div class="contact_form_title d-flex justify-content-center">Haz tu Pago</div>
+              <form>
+                <div id="paypal-buttons"></div>
+                  <script>
+                    paypal.Buttons({
+                      createOrder: function(data, actions) {
+                        return actions.order.create({
+                          purchase_units: [{
+                            amount: {
+                              value: '{{$totalUSD}}'
+                            }
+                          }]
+                        });
+                      },
+                      onApprove: function(data, actions) {
+                        return actions.order.capture().then(function(details) {
+                          alert('Transacción completada por ' + details.payer.name.given_name);
+                        });
+                      },
+                      onError: function(err) {
+                        console.error(err);
+                      }
+                    }).render('#paypal-buttons');
+                  </script>
                 </div>
+              </form>
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  <!-- /Payment Form -->
 
 @endsection
