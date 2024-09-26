7c2ac5ba09a63e2ae064da32ba9b3456ebab3591
Rodrigo Morales Varela <moralesvarelarodrigo0@gmail.com>
2024-09-25 22:11:24 -0500

    Estilos vista de pagos

--- resources/views/payment/payment.blade.php
+++ resources/views/payment/payment.blade.php
@@ -1,30 +1,50 @@
-@extends('layouts.base')
+@extends('layouts.blank')
+
+@section('title', 'Realiza tu pago')
 
 @section('css')
-  <script src="https://www.paypal.com/sdk/js?client-id={{config('app.paypal.id')}}&currency=USD"></script>
+<link rel="stylesheet" type="text/css" href="{{ url('styles/payment_styles.css') }}">
+<script src="https://www.paypal.com/sdk/js?client-id={{config('app.paypal.id')}}&currency=USD"></script>
 @endsection
 
 @section('content')
-  <div id="paypal-buttons"></div>
-    <script>
-      paypal.Buttons({
-        createOrder: function(data, actions) {
-          return actions.order.create({
-            purchase_units: [{
-              amount: {
-                value: '{{$totalUSD}}'
-              }
-            }]
-          });
-        },
-        onApprove: function(data, actions) {
-          return actions.order.capture().then(function(details) {
-            alert('Transacción completada por ' + details.payer.name.given_name);
-          });
-        },
-        onError: function(err) {
-          console.error(err);
-        }
-      }).render('#paypal-buttons');
-    </script>
+
+    <!-- Login Form -->
+    <div class="contact_form" style="height: calc(75vh - 110px )">
+        <div class="container" >
+            <div class="row">
+                <div class="col-lg-10 offset-lg-1">
+                    <div class="contact_form_container" >
+                        <div class="contact_form_title d-flex justify-content-center">Haz tu Pago</div>
+                        <form>
+                          <div id="paypal-buttons"></div>
+                          <script>
+                            paypal.Buttons({
+                              createOrder: function(data, actions) {
+                                return actions.order.create({
+                                  purchase_units: [{
+                                    amount: {
+                                      value: '{{$totalUSD}}'
+                                    }
+                                  }]
+                                });
+                              },
+                              onApprove: function(data, actions) {
+                                return actions.order.capture().then(function(details) {
+                                  alert('Transacción completada por ' + details.payer.name.given_name);
+                                });
+                              },
+                              onError: function(err) {
+                                console.error(err);
+                              }
+                            }).render('#paypal-buttons');
+                          </script>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
